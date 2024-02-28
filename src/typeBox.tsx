ecacbe36063322e82f855f0a1e473e7da8fdf4db
FeiserR <160156176+FeiserR@users.noreply.github.com>
2024-02-28 15:39:14 -0300

    added the small tsu

--- src/typeBox.tsx
+++ src/typeBox.tsx
@@ -3,7 +3,6 @@ import React, {useState} from 'react'
 
 function TypeBox() {
 
-
   const [input, setInput] = useState('')
 
   function handleInputChange (event: React.ChangeEvent<HTMLInputElement>) { 
@@ -13,7 +12,7 @@ function TypeBox() {
   function transformString (input: string) {
 
     const transformations: {[key: string]: string} = {
-      a: 'あ', e: 'え', i: 'い', u: 'う', o: 'お', ka: 'か', sa: 'さ', ta: 'た', na: 'な', ha: 'は', ma: 'ま', ya: 'や', ra: 'ら', wa: 'わ', nn: 'ん', ki: 'き', shi: 'し', chi: 'ち', ni: 'に', hi: 'ひ', mi: 'み', ri: 'り',ku: 'く', su: 'す', tsu: 'つ', nu: 'ぬ', fu: 'ふ', mu: 'む', yu: 'ゆ', ru: 'る',ke: 'け', se: 'せ', te: 'て', ne: 'ね', he: 'へ', me: 'め', re: 'れ',ko: 'こ', so: 'そ', to: 'と', no: 'の', ho: 'ほ', mo: 'も', yo: 'よ', ro: 'ろ',ga: 'が', za: 'ざ', da: 'だ', ba: 'ば', pa: 'ぱ',gi: 'ぎ', ji: 'じ', bi: 'び', pi: 'ぴ',gu: 'ぐ', zu: 'ず', du: 'づ', dsu: 'づ', bu: 'ぶ', pu: 'ぷ',ge: 'げ', ze: 'ぜ', de: 'で', be: 'べ', pe: 'ぺ',go: 'ご', zo: 'ぞ', do: 'ど', bo: 'ぼ', po: 'ぽ',kya: 'きゃ', sha: 'しゃ', cha: 'ちゃ', nya: 'にゃ', hya: 'ひゃ', mya: 'みゃ', rya: 'りゃ',kyu: 'きゅ', shu: 'しゅ', chu: 'ちゅ', nyu: 'にゅ', hyu: 'ひゅ', myu: 'みゅ', ryu: 'りゅ',kyo: 'きょ', sho: 'しょ', cho: 'ちょ', nyo: 'にょ', hyo: 'ひょ', myo: 'みょ', ryo: 'りょ',bya: 'びゃ', pya: 'ぴゃ',byu: 'びゅ', pyu: 'ぴゅ',byo: 'びょ', pyo: 'ぴょ',ja: 'じゃ', ju: 'じゅ', jo: 'じょ', 
+      a: 'あ', e: 'え', i: 'い', u: 'う', o: 'お', ka: 'か', sa: 'さ', ta: 'た', na: 'な', ha: 'は', ma: 'ま', ya: 'や', ra: 'ら', wa: 'わ', nn: 'ん', ki: 'き', shi: 'し', chi: 'ち', ni: 'に', hi: 'ひ', mi: 'み', ri: 'り',ku: 'く', su: 'す', tsu: 'つ', nu: 'ぬ', fu: 'ふ', mu: 'む', yu: 'ゆ', ru: 'る',ke: 'け', se: 'せ', te: 'て', ne: 'ね', he: 'へ', me: 'め', re: 'れ',ko: 'こ', so: 'そ', to: 'と', no: 'の', ho: 'ほ', mo: 'も', yo: 'よ', ro: 'ろ',ga: 'が', za: 'ざ', da: 'だ', ba: 'ば', pa: 'ぱ',gi: 'ぎ', ji: 'じ', bi: 'び', pi: 'ぴ',gu: 'ぐ', zu: 'ず', du: 'づ', dsu: 'づ', bu: 'ぶ', pu: 'ぷ',ge: 'げ', ze: 'ぜ', de: 'で', be: 'べ', pe: 'ぺ',go: 'ご', zo: 'ぞ', do: 'ど', bo: 'ぼ', po: 'ぽ',kya: 'きゃ', sha: 'しゃ', cha: 'ちゃ', nya: 'にゃ', hya: 'ひゃ', mya: 'みゃ', rya: 'りゃ',kyu: 'きゅ', shu: 'しゅ', chu: 'ちゅ', nyu: 'にゅ', hyu: 'ひゅ', myu: 'みゅ', ryu: 'りゅ',kyo: 'きょ', sho: 'しょ', cho: 'ちょ', nyo: 'にょ', hyo: 'ひょ', myo: 'みょ', ryo: 'りょ',bya: 'びゃ', pya: 'ぴゃ',byu: 'びゅ', pyu: 'ぴゅ',byo: 'びょ', pyo: 'ぴょ',ja: 'じゃ', ju: 'じゅ', jo: 'じょ', kka: 'っか', kki: 'っき', kku: 'っく', kke: 'っけ', kko: 'っこ', kkya: 'っきゃ', kkyu: 'っきゅ', kkyo: 'っきょ', gga: 'っが', ggi: 'っぎ', ggu: 'っぐ', gge: 'っげ', ggo: 'っご', gya: 'っぎゃ', gyu: 'っぎゅ', gyo: 'っぎょ', ssa: 'っさ', ssi: 'っし', ssu: 'っす', sse: 'っせ', sso: 'っそ', ssha: 'っしゃ', sshu: 'っしゅ', ssho: 'っしょ', tta: 'った', tti: 'っち', ttu: 'っつ', tte: 'って', tto: 'っと', ccha: 'っちゃ', cchu: 'っちゅ', ccho: 'っちょ', nna: 'っな', nni: 'っに', nnu: 'っぬ', nne: 'っね', nno: 'っの', nnya: 'っにゃ', nnyu: 'っにゅ', nnyo: 'っにょ', hha: 'っは', hhi: 'っひ', hhu: 'っふ', hhe: 'っへ', hho: 'っほ', hhya: 'っひゃ', hhyu: 'っひゅ', hhyo: 'っひょ', mma: 'っま', mmi: 'っみ', mmu: 'っむ', mme: 'っめ', mmo: 'っも', mmya: 'っみゃ', mmyu: 'っみゅ', mmyo: 'っみょ', rra: 'っら', rri: 'っり', rru: 'っる', rre: 'っれ', rro: 'っろ', rrya: 'っりゃ', rryu: 'っりゅ', rryo: 'っりょ', ggya: 'っぎゃ', ggyu: 'っぎゅ', ggyo: 'っぎょ', jja: 'っじゃ', jju: 'っじゅ', jjo: 'っじょ', bba: 'っば', bbi: 'っび', bbu: 'っぶ', bbe: 'っべ', bbo: 'っぼ', bbya: 'っびゃ', bbyu: 'っびゅ', bbyo: 'っびょ', ppa: 'っぱ', ppi: 'っぴ', ppu: 'っぷ', ppe: 'っぺ', ppo: 'っぽ', ppya: 'っぴゃ', ppyu: 'っぴゅ', ppyo: 'っぴょ'
     }
   
     // removes hiragana and katakana characters from the input//
@@ -34,7 +33,6 @@ function TypeBox() {
     }
 
   }
-
 
   return (
     <>
